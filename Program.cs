4840c656097d2ee4da7c2baabe9f1822a68d7d71
Milagro Scardigno <mdscardigno@gmail.com>
2021-10-04 16:22:41 -0400

    Created menu and conditions to manipulate input

--- Program.cs
+++ Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SuncoastHumanResources
 {
+  class Employee{
+    public string Name {get; set;}
+    public int Department {get; set;}
+    public int Salary {get; set;}
+    public int MonthlySalary(){
+      return Salary / 12;
+    }
+  }
   class Program
   {
+    static void DisplayGreeting(){
+      Console.WriteLine("-------------------------------------------");
+      Console.WriteLine("Welcome to Suncoast Human Resources.");
+      Console.WriteLine("-------------------------------------------");
+      Console.WriteLine();
+      Console.WriteLine();
+      }
+      static string PromptForString(string prompt){
+        Console.Write(prompt);
+        var userInput = Console.ReadLine();
+        return userInput;
+      }
+      static int PromptForInteger(string prompt){
+        var userInput = 0;
+        Console.Write(prompt);
+        var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+        if(isThisGoodInput){
+          return userInput;
+        }
+        else
+        {
+          Console.WriteLine("Sorry, that isn't a valid input. I am using 0 as your answer.");
+          return 0;
+        }
+      }
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var employees = new List<Employee>();
+      var employee = new Employee();
+      DisplayGreeting();
+      //MENU
+      var keepGoing = true;
+      while (keepGoing)
+      {
+        Console.WriteLine();
+        Console.WriteLine("What do you want to do? (A)dd an employee or (S)ow all the employees or (F)ind an employee or (Q)uit?: ");
+        var choice = Console.ReadLine().ToUpper();
+        if(choice == "Q"){
+          //User says to quit therefore we set keepGoing to false.
+          keepGoing = false;
+        }
+        else
+        {
+          employee.Name = PromptForString("What is your name?: ");
+          employee.Department = PromptForInteger("What is your department number?: ");
+          employee.Salary = PromptForInteger("What is your yearly salary?: ");
+          Console.WriteLine($"Hello, {employee.Name} makes {employee.MonthlySalary()} dollars per month.");
+          employees.Add(employee);
+        }
+      }
+      
     }
   }
 }
