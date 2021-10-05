cb758548ce6482407be5bcae2404bca5af64bae8
Milagro Scardigno <mdscardigno@gmail.com>
2021-10-05 16:20:03 -0400

    Employee class changes

--- Program.cs
+++ Program.cs
@@ -3,87 +3,119 @@ using System.Collections.Generic;
 
 namespace SuncoastHumanResources
 {
-  class Employee{
-    public string Name {get; set;}
-    public int Department {get; set;}
-    public int Salary {get; set;}
-    public int MonthlySalary(){
+  class Employee
+  {
+    public string Name { get; set; }
+    public int Department { get; set; }
+    public int Salary { get; set; }
+    public int MonthlySalary()
+    {
       return Salary / 12;
-    }//end of MonthlySalary function
-  }//end of employee class
+    }
+  }
+
   class Program
   {
-    static void DisplayGreeting(){
-      Console.WriteLine("-------------------------------------------");
-      Console.WriteLine("Welcome to Suncoast Human Resources.");
-      Console.WriteLine("-------------------------------------------");
+    static void DisplayGreeting()
+    {
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine("    Welcome to Our Employee Database    ");
+      Console.WriteLine("----------------------------------------");
       Console.WriteLine();
       Console.WriteLine();
-      }
-      static string PromptForString(string prompt){
-        Console.Write(prompt);
-        var userInput = Console.ReadLine();
+    }
+
+    static string PromptForString(string prompt)
+    {
+      Console.Write(prompt);
+      var userInput = Console.ReadLine();
+
+      return userInput;
+    }
+
+    static int PromptForInteger(string prompt)
+    {
+      Console.Write(prompt);
+      int userInput;
+      var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+      if (isThisGoodInput)
+      {
         return userInput;
       }
-      static int PromptForInteger(string prompt){
-        var userInput = 0;
-        Console.Write(prompt);
-        var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
-        if(isThisGoodInput){
-          return userInput;
-        }
-        else
-        {
-          Console.WriteLine("Sorry, that isn't a valid input. I am using 0 as your answer.");
-          return 0;
-        }
+      else
+      {
+        Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+        return 0;
       }
+    }
+
     static void Main(string[] args)
     {
-      //a list of employees
+      // Our list of employees
       var employees = new List<Employee>();
-      //MENU
+
+      // Should we keep showing the menu?
+
       var keepGoing = true;
-      //we display the greeting.
+
       DisplayGreeting();
-      //while the user hasn't said quit yet
-      while (keepGoing)
-      {
-        //insert a blank line then
+
+      // While the user hasn't said QUIT yet
+      while (keepGoing) {
+        // Insert a blank line then prompt them and get their answer (force uppercase)
         Console.WriteLine();
-        //prompt them and get their answer (force uppercase)
-        Console.WriteLine("What do you want to do? (A)dd an employee or (S)ow all the employees or (F)ind an employee or (Q)uit?: ");
+        Console.Write("What do you want to do? (A)dd an employee or (S)how all the employees or (F)ind an employee or (Q)uit: ");
         var choice = Console.ReadLine().ToUpper();
-        if(choice == "Q"){
-          //User says to quit therefore we set keepGoing to false.
+
+        if (choice == "Q") {
+          // They said quit, so set our keepGoing to false
           keepGoing = false;
-        }else if(choice == "S"){
-          //Loop through each employee
-          foreach (var employee in employees)
-          {
-            //print all the details
-            Console.WriteLine($"{employee.Name} is in department {employee.Department} and makes {employee.Salary}.");
-          }
-        }else if(choice == "F"){
-          //ask for the name of an employee
+        } else if (choice == "F") {
+          // Ask for the name of an employee
           var name = PromptForString("What name are you looking for? ");
-          //Make a new variable to store the found employee, initializing to null which will indicate no match found
+
+          // Make a new variable to store the found employee, initializing
+          // to null which will indicate no match found
           Employee foundEmployee = null;
-          //go through all the employees   
-        }
-        else
-        {
-          //make a new employee object
+
+          // Go through all the employees
+          foreach(var employee in employees) {
+            // If the name matches
+            if (employee.Name == name) {
+              // ... then store this employee in the foundEmployee variable
+              foundEmployee = employee;
+            }
+          }
+
+          // If the foundEmployee is still null, nothing was found
+          if (foundEmployee == null) {
+            Console.WriteLine("No match found");
+          } else {
+            // Otherwise print details of the found employee
+            Console.WriteLine($"{foundEmployee.Name} is in department {foundEmployee.Department} and makes ${foundEmployee.Salary}");
+          }
+        } else if (choice == "S") {
+          // Loop through each employee
+          foreach(var employee in employees) {
+            // And print details
+            Console.WriteLine($"{employee.Name} is in department {employee.Department} and makes ${employee.Salary}");
+          }
+        } else {
+          // Make a new employee object
           var employee = new Employee();
-          //Prompt for values and save them in the employees properties.
-          employee.Name = PromptForString("What is your name?: ");
-          employee.Department = PromptForInteger("What is your department number?: ");
-          employee.Salary = PromptForInteger("What is your yearly salary?: ");
-          Console.WriteLine($"Hello, {employee.Name} makes {employee.MonthlySalary()} dollars per month.");
-          //Add it to the list
+
+          // Prompt for values and save them in the employee's properties
+          employee.Name = PromptForString("What is your name? ");
+          employee.Department = PromptForInteger("What is your department number? ");
+          employee.Salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+          // Add it to the list
           employees.Add(employee);
         }
-      }//end of while statement keepGoing
-    }//end of main
-  }//end of class Program
+
+        // end of the `while` statement
+      }
+    }
+  }
 }
