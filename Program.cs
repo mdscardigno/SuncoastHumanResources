f56eb57f12ac19a22bec3e4021ec30e9e8eb5367
Milagro Scardigno <mdscardigno@gmail.com>
2021-10-05 14:19:02 -0400

    Added missing braket in one of the else ifs

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace SuncoastHumanResources
     public int Salary {get; set;}
     public int MonthlySalary(){
       return Salary / 12;
-    }
-  }
+    }//end of MonthlySalary function
+  }//end of employee class
   class Program
   {
     static void DisplayGreeting(){
@@ -40,30 +40,50 @@ namespace SuncoastHumanResources
       }
     static void Main(string[] args)
     {
+      //a list of employees
       var employees = new List<Employee>();
-      var employee = new Employee();
-      DisplayGreeting();
       //MENU
       var keepGoing = true;
+      //we display the greeting.
+      DisplayGreeting();
+      //while the user hasn't said quit yet
       while (keepGoing)
       {
+        //insert a blank line then
         Console.WriteLine();
+        //prompt them and get their answer (force uppercase)
         Console.WriteLine("What do you want to do? (A)dd an employee or (S)ow all the employees or (F)ind an employee or (Q)uit?: ");
         var choice = Console.ReadLine().ToUpper();
         if(choice == "Q"){
           //User says to quit therefore we set keepGoing to false.
           keepGoing = false;
+        }else if(choice == "S"){
+          //Loop through each employee
+          foreach (var employee in employees)
+          {
+            //print all the details
+            Console.WriteLine($"{employee.Name} is in department {employee.Department} and makes {employee.Salary}.");
+          }
+        }else if(choice == "F"){
+          //ask for the name of an employee
+          var name = PromptForString("What name are you looking for? ");
+          //Make a new variable to store the found employee, initializing to null which will indicate no match found
+          Employee foundEmployee = null;
+          //go through all the employees   
         }
         else
         {
+          //make a new employee object
+          var employee = new Employee();
+          //Prompt for values and save them in the employees properties.
           employee.Name = PromptForString("What is your name?: ");
           employee.Department = PromptForInteger("What is your department number?: ");
           employee.Salary = PromptForInteger("What is your yearly salary?: ");
           Console.WriteLine($"Hello, {employee.Name} makes {employee.MonthlySalary()} dollars per month.");
+          //Add it to the list
           employees.Add(employee);
         }
-      }
-      
-    }
-  }
+      }//end of while statement keepGoing
+    }//end of main
+  }//end of class Program
 }
